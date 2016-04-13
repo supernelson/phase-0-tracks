81937d1cc5a389ecbc946e76ea71d8b305276f5c
Nelson Chen <nelsonchen1020@gmail.com>
2016-04-12 17:20:32 -0700

    stuck and debugging adding nil to hamsters estimated age on line 16

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -1,1 +1,20 @@
 puts "what is the hamster's name?"
+  name = gets.chomp.to_s
+puts "what is the volume level of this hamster from 1 to 10?"
+  noise = gets.chomp.to_f
+puts "what is its fur color?"
+  fur_color = gets.chomp.to_s
+puts "is this hamster a good candidate for adoption (yes or no)?"
+  adoptability = gets.chomp.to_s
+  if adoptability == "yes"
+    adoptability = true
+  else
+    adoptability = false
+end
+puts "what is this hamster's estimated age?"
+  age = gets.chomp
+  if age == ""
+    puts nil
+  else
+    puts age.to_i
+end
