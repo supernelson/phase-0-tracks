cce3b55bc6f6dba1697060340fb2a491a9bfed9f
Nelson Chen <nelsonchen1020@gmail.com>
2016-04-20 10:43:17 -0700

    started 5.3

---
+++ ruby/hashes.rb
@@ -0,0 +1,11 @@
+interior_designer = {
+  name: "nelson chen",
+  age: 30,
+  children: 0,
+  decor_theme: "warm minimalist",
+  paint: "color psychology",
+  furniture: "functional",
+}
+
+brian = interior_designer.new
+puts brian
