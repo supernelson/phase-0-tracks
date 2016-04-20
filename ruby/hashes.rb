4cb9127d40131015c2cfc054eb3f1b33bf877fe5
Nelson Chen <nelsonchen1020@gmail.com>
2016-04-20 12:41:22 -0700

    adding changes for going back to change an answer

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -1,11 +1,34 @@
 interior_designer = {
-  name: "nelson chen",
-  age: 30,
-  children: 0,
-  decor_theme: "warm minimalist",
-  paint: "color psychology",
-  furniture: "functional",
+  name: "",
+  age: "",
+  children:"",
+  decor_theme: "",
+  paint: "",
+  furniture: "",
 }
+puts "what is your name?"
+interior_designer[:name] << gets.chomp.to_s
+puts "what is your age?"
+interior_designer[:age] << gets.chomp.to_s
+puts "how many children do you have?"
+interior_designer[:children] << gets.chomp.to_s
+puts "what is your favorite decor theme?"
+interior_designer[:decor_theme] << gets.chomp.to_s
+puts "what's your favorite paint style?"
+interior_designer[:paint] << gets.chomp.to_s
+puts "whats furniture style do you like best?"
+interior_designer[:furniture] << gets.chomp.to_s
+puts interior_designer
 
-brian = interior_designer.new
-puts brian
+puts "Would you like to change any of your answer?"
+change = gets.chomp.downcase
+if change == "yes"
+  puts "pick one: name, age, children, decor_theme, paint, furniture."
+  symbol = ":" + gets.chomp.downcase
+  puts "what is your new answer?"
+  new_answer = gets.chomp
+  interior_designer[symbol] << new_answer
+  puts interior_designer
+else change == "none"
+  puts interior_designer
+end
