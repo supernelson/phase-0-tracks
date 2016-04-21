d0ee7af298ebdd039714102abb120e2469feefa0
Nelson Chen <nelsonchen1020@gmail.com>
2016-04-21 13:39:34 -0700

    finished 5.4 release 1

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -1,3 +1,4 @@
+#5.4 release 0 prework
 def castro
   puts "here are some spots to visit in the castro."
   shops = {coffee: "reveille", food: "lark", bites: "anchor oyster bar", food: "starbelly"}
@@ -8,9 +9,21 @@ def castro
       puts "you should go to #{name}"
     elsif type == :bites
       puts "hot tip: check out #{name}"
-      puts "quit"
+    else
+      puts "nothing found."
     end
   end
 end
-
 castro
+
+#release 1
+sport_type = ["soccer", "basketball"]
+sport_team = {barcelona: "messi", warriors: "curry", madrid: "ronaldo"}
+
+sport_type.each {|sport| puts "#{sport} is a sport"}
+sport_type.map! do |a| puts "#{a.capitalize}"
+end
+sport_team.each {|team, player| puts "The best player on #{team} is #{player}."}
+sport_team.map do |team, player| puts "#{player.upcase} is the leader of #{team.upcase}"
+  end
+print sport_team
