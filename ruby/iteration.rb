8bfcfa7ffcf7d9feecad163cf6b2461f525819c7
Nelson Chen <nelsonchen1020@gmail.com>
2016-04-21 15:51:01 -0700

    completed 5.4 with Spencer, used reject for the final question because it was the closest method we could find.

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -29,21 +29,53 @@ sport_team.map do |team, player| puts "#{player.upcase} is the leader of #{team.
 print sport_team
 
 #release 2
-countries = ["france", "spain", "germany", "greece"]
+countries = ["France", "spain", "America", "Germany", "greece"]
 country_city = {france: "Paris", spain: "madrid", germany: "Berlin", greece: "athens", america: "Washington", iran: "ternan", iraq: "Baghdad" }
 
 #1 A method that iterates through the items, deleting any that meet a certain condition (for example, deleting any numbers that are less than 5).
 
-countries.delete_if {|country| country.include?("s")}
+countries.delete_if {|country| country.include?("g")}
+country_city.delete_if {|country, city| city.include?("h") && country.to_s.include?("g")}
 p countries
-
-country_city.delete_if {|country, city| city.include?("h")|| country.to_s.include?("g")}
 p country_city
 
-#spencer side testing
-country_city.delete_if {|country, city| country.to_s.length > 5}
-p country_city
 
 #2 A method that filters a data structure for only items that do satisfy a certain condition (for example, keeping any numbers that are less than 5).
+#reseting array
+countries = ["France", "spain", "America", "Germany", "greece"]
+country_city = {france: "Paris", spain: "madrid", germany: "Berlin", greece: "athens", america: "Washington", iran: "ternan", iraq: "Baghdad" }
 
-countries.keep_if {|country| country.
+countries.keep_if {|country| country.include?("a")}
+p countries
+country_city.keep_if do |country, city|
+  country.to_s.length > 5 && city.include?("a")
+end
+p country_city
+
+#3 A different method that filters a data structure for only items satisfying a certain condition -- Ruby offers several options!
+#reseting array
+countries = ["France", "spain", "America", "Germany", "greece"]
+country_city = {france: "Paris", spain: "madrid", germany: "Berlin", greece: "athens", america: "Washington", iran: "ternan", iraq: "Baghdad" }
+
+countries.select {|country| country.include?("a")}
+p countries
+country_city.select do |country, city|
+  country.to_s.length > 5 && city.include?("a")
+end
+
+#4 A method that will remove items from a data structure until the condition in the block evaluates to false, then stops.
+#reseting array
+countries = ["France", "spain", "America", "Germany", "greece"]
+country_city = {france: "Paris", spain: "madrid", germany: "Berlin", greece: "athens", america: "Washington", iran: "ternan", iraq: "Baghdad" }
+
+countries.drop_while do |a| a.length <= 6
+end
+p countries
+
+new_countries = countries.drop_while do |a| a.length <= 6
+end
+p new_countries
+
+new_list = country_city.reject do |country, city| country.to_s.include?("g")
+end
+p new_list
