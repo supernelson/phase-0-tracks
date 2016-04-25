bf522543ba718149c1c6565ca7abf64d8f231f2a
Nelson Chen <nelsonchen1020@gmail.com>
2016-04-25 08:22:01 -0700

    pushing files to turn in

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -4,38 +4,58 @@
 # create an index of vowels
 # if a vowel is encountered use .next on the vowel array
 
-def spy_name_flip
-  real_name = 'nelson chen' #replace with gets.chomp
-  spy_name = real_name.split(' ')
-  spy_name[0], spy_name[1] = spy_name[1], spy_name[0]
-  spy_name = spy_name.join(' ').chars
-  #return spy_name
+def split_flip_name
+  p "what is your first and last name? (type 'print' to quit)"
+  name = gets.chomp.downcase
+  old_name_array = []
+  old_name_array << name
+  p old_name_array
+  split_name = name.split(' ')
+  flip_name = split_name[1], split_name[0]
+  #p flip_name
+  join_name = flip_name.join(' ').chars
+  #p join_name
 end
-spy_name_flip
 
-#loop through the spy_names
+#loop through the flip_names
 #make vowels in a position
 #flip the vowels to next vowel
 #if vowel... then add 1 to index.
 
-def letter_change(spy_name_flip)
-  p spy_name_flip.length
-  p spy_name_flip
-  spy_name_encrypted = ""
-  vowel = 'a'||'e'||'i'||'o'||'u'
-  consonant = 'b'||'c'||'d'||'f'||'g'||'h'||'j'||'k'||'l'||'m'||'n'||'p'||'q'||'r'||'s'||'t'||'v'||'w'||'x'||'y'||'z'
-  i = 0
-  while i < spy_name_flip.length
-    if spy_name_flip[i].include?('c')
-      spy_name_flip[i].next!
-    elsif spy_name_flip[i]=='a' || spy_name_flip[i]=='e' || spy_name_flip[i]=='i' || spy_name_flip[i]=='o' || spy_name_flip[i]=='u'
-      spy_name_flip[i] = "a"
-    elsif spy_name_flip[i] == ' '
-      spy_name_flip[i]
+def letter_change
+  consonant = "bcdfghjklmnpqrstvwxyz"
+  vowel = "aeiou"
+  new_name = ""
+  letter_new = ""
+
+    split_flip_name.each do |letter|
+      if consonant.index(letter) != nil
+        letter_new = consonant[consonant.index(letter) + 1]
+      elsif vowel.index(letter) != nil
+        if letter == "u" #addresses last vowel edge case
+          letter_new = "a"
+        elsif
+          letter_new = vowel[vowel.index(letter) + 1]
+        end
+      elsif letter == " "
+        letter_new = letter
+      end
+      new_name += letter_new.to_s #.to_s addresses a nil edge case
     end
-    i += 1
-  end
-  p spy_name_flip
+    p new_name
 
 end
-letter_change(spy_name_flip)
+
+def name_array
+  new_name_array = []
+  until split_flip_name == [" ", "q", "u", "i", "t"] #exits when quit is entered.
+    new_name_array << letter_change
+    p new_name_array
+  end
+end
+name_array
+
+
+#why is everything printing so many times?
+#how do i use info from one method and apply to another?
+#how do i keep saving more gets.chomp into an array?
