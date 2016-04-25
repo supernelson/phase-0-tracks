7fff5887c2918ef61ba1bbca2476e1e471cddc0d
Nelson Chen <nelsonchen1020@gmail.com>
2016-04-24 21:37:21 -0700

    stored names in array. BUG: still asks twice for the name.

--- ruby/alias/alias_manager.rb
+++ ruby/alias/alias_manager.rb
@@ -4,15 +4,14 @@
 # create an index of vowels
 # if a vowel is encountered use .next on the vowel array
 
-def name_flip
-  real_name = gets.chomp #replace with gets.chomp
-  flip_name = real_name.split(' ')
-  p flip_name
-  flip_name = flip_name[1], flip_name[0]
-  p flip_name
-  flip_name = flip_name.join(' ').chars
-  p flip_name
-  return flip_name
+def split_flip_name
+  p "what is your first and last name? (type 'print' to quit)"
+  name = gets.chomp.downcase
+  split_name = name.split(' ')
+  flip_name = split_name[1], split_name[0]
+  #p flip_name
+  join_name = flip_name.join(' ').chars
+  #p join_name
 end
 
 #loop through the flip_names
@@ -20,34 +19,40 @@ end
 #flip the vowels to next vowel
 #if vowel... then add 1 to index.
 
-def letter_change(name)
-  alphabet = "bcdfghjklmnpqrstvwxyz"
+def letter_change
+  consonant = "bcdfghjklmnpqrstvwxyz"
   vowel = "aeiou"
-  new_name = ""
   name_array = []
+  new_name = ""
   letter_new = ""
 
-  name.map! do |letter|
-    if alphabet.index(letter) != nil
-      letter_new = alphabet[alphabet.index(letter) + 1]
-    elsif vowel.index(letter) != nil
-      letter_new = vowel[vowel.index(letter) + 1]
-    elsif letter == " "
-      letter_new = letter
+    split_flip_name.map do |letter|
+      if consonant.index(letter) != nil
+        letter_new = consonant[consonant.index(letter) + 1]
+      elsif vowel.index(letter) != nil
+        if vowel == "u" #addresses last vowel edge case
+          letter_new == "a"
+        elsif
+          letter_new = vowel[vowel.index(letter) + 1]
+        end
+      elsif letter == " "
+        letter_new = letter
+      end
+      new_name += letter_new.to_s #.to_s addresses a nil edge case
     end
-    new_name += letter_new
-  end
-  name_array << new_name
-  p name_array
-  return name_array
-end
-letter_change(name_flip)
+    p new_name
 
-def all_names(name_flip)
-  if name_flip.real_name == "quit"
-    p letter_change
-  #end
 end
+
+def name_array
+  name_array = []
+  until split_flip_name == [" ", "q", "u", "i", "t"]
+    name_array << letter_change
+    p name_array
+  end
+end
+name_array
+
 
 #why is everything printing so many times?
 #how do i use info from one method and apply to another?
