ba118e5e4e575b002d4b4f26d07b92dc08ebde4c
Nelson Chen <nelsonchen1020@gmail.com>
2016-04-27 16:59:11 -0700

    finished release 1

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,4 +1,10 @@
 module Shout
-  def method1
+  def self.yell_angrily(words)
+    words + "!!!" + " :("
+  end
+  def self.yelling_happily(words)
+    words + "! Yes yes yes yes yes!!! We did it! :)"
   end
 end
+p Shout.yell_angrily("nooooo")
+p Shout.yelling_happily("WHOOOOOOOO HOOOOO")
