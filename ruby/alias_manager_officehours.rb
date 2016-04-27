1cd8fc1800f6e77b29e743ea1ed8ec35c3afa6ed
Nelson Chen <nelsonchen1020@gmail.com>
2016-04-27 11:52:40 -0700

    finished puppy.rb with Ed Kim, updated my Santa and Alias Managers after office hours.

--- ruby/alias_manager_officehours.rb
+++ ruby/alias_manager_officehours.rb
@@ -1,3 +1,5 @@
+##TYLER OFFICE HOURS
+
 # gets.chomp the spy name
 # split the name at the space for first & last name
 # switch the positions
@@ -6,6 +8,10 @@
 
 def start_name
   gets.chomp.downcase
+  # name = gets.chomp.downcase
+  # if name == "quit" || name == ''
+  #   break
+  # end
 end
 
 def split_name(name)
@@ -52,7 +58,9 @@ end
 def name_array
   new_name_array = []
   spy_name = split_flip_name
-  until spy_name == [" ", "q", "u", "i", "t"] #exits when quit is entered.
+  #exits when quit is entered.
+  until spy_name == [" ", "q", "u", "i", "t"]
+  loop do
     new_name_array << letter_change(spy_name)
     p new_name_array
   end
@@ -63,3 +71,93 @@ name_array
 #why is everything printing so many times?
 #how do i use info from one method and apply to another?
 #how do i keep saving more gets.chomp into an array?
+
+
+
+
+## ANDREW OFFICE HOURS
+
+# # gets.chomp the spy name
+# # split the name at the space for first & last name
+# # switch the positions
+# # create an index of vowels
+# # if a vowel is encountered use .next on the vowel array
+
+
+# def split_name(name)
+#   name.split(' ')
+# end
+
+# def split_flip_name(name)
+#   p "what is your first and last name? (type 'print' to quit)"
+
+#   formatted_name = split_name(name).reverse.join(' ').chars
+# end
+
+# # split_flip_name = 'torres felicia'.chars
+# #loop through the flip_names
+# #make vowels in a position
+# #flip the vowels to next vowel
+# #if vowel... then add 1 to index.
+
+# def letter_change(spy_name)
+#   consonant = "bcdfghjklmnpqrstvwxyz"
+#   vowel = "aeiou"
+#   new_name = ""
+#   letter_new = ""
+
+#     spy_name.each do |letter|
+#       if consonant.index(letter) != nil
+#         letter_new = consonant[consonant.index(letter) + 1]
+#       elsif vowel.index(letter) != nil
+#         if letter == "u" #addresses last vowel edge case
+#           letter_new = "a"
+#         elsif
+#           letter_new = vowel[vowel.index(letter) + 1]
+#         end
+#       elsif letter == " "
+#         letter_new = letter
+#       end
+#       new_name += letter_new.to_s #.to_s addresses a nil edge case
+#     end
+#     new_name
+
+# end
+
+# # p letter_change(split_flip_name) = 'vussit gimodoe'
+
+
+
+# loop do
+#   puts 'what is the spys name'
+#   input = gets.chomp.downcase
+
+#   if input == 'quit'
+#     break
+#   end
+
+#   formatted_name = split_flip_name(input)
+
+#   secret_name = letter_change(formatted_name)
+# end
+
+
+
+# # def name_array
+# #   new_name_array = []
+# #   spy_name = split_flip_name
+# #   #exits when quit is entered.
+# #   until spy_name == [" ", "q", "u", "i", "t"]
+# #   loop do
+# #     new_name_array << letter_change(spy_name)
+# #     p new_name_array
+# #   end
+# # end
+
+
+# # name_array
+
+
+# #why is everything printing so many times?
+# #how do i use info from one method and apply to another?
+# #how do i keep saving more gets.chomp into an array?
--- ruby/santa.rb
+++ ruby/santa.rb
@@ -27,8 +27,11 @@ class Santa
 
   def get_mad_at(reindeer_name)
    p @reindeer_rank << @reindeer_rank[@reindeer_rank.index(reindeer_name)]
-   #p @reindeer_rank.delete(reindeer_name) #this one deletes both the old and the new name
-   p @reindeer_rank.index(reindeer_name) #why does this pickup only 1 and not the new one at position last?
+   #this one deletes both the old and the new name
+   #p @reindeer_rank.delete(reindeer_name)
+
+   #why does this pickup only 1 and not the new one at position last?
+   p @reindeer_rank.index(reindeer_name)
    p @reindeer_rank.delete_at(@reindeer_rank.index(reindeer_name))
    p @reindeer_rank
   end
@@ -57,37 +60,153 @@ nelson = Santa.new("nelson", "male", "asian")
 nelson.eat_milk_and_cookies
 nelson.speak
 
+# santas = []
+# santas << Santa.new("james", "male", "black")
+# santas << Santa.new("george", "male", "Latino")
+# santas << Santa.new("andy", "bigender", "white")
+# santas << Santa.new("stephanie", "female", "Japanese")
+# santas << Santa.new("laura", "female", "prefer not to say")
+# santas << Santa.new("jaden", "gender fluid", "Mystical Creature (unicorn)")
+# santas << Santa.new("Bob", "N/A", "N/A")
+# #p santas
+# p santas.length #7
+
+# timing = 0
+# 2.times do
+#   i = 0 #resets 1 on second times
+#   if timing > 0
+#     p santas[i].name
+#     p santas[i].celebrate_birthday
+#   end
+
+#   until i >= santas.length
+#     p santas[i].name
+#     p santas[i].celebrate_birthday
+#     i += 1
+#   end
+#    timing += 1
+# end
+
+# #doesn't work on existing santas because enumerator issues...
+# rudy = Santa.new("rudy", "male", "asian")
+# rudy.get_mad_at("Prancer") #no method error
+# #calling my setter method
+# p rudy.name = "homer"
+# p rudy.age
+# p rudy.ethnicity
+
 santas = []
-santas << Santa.new("james", "male", "black")
-santas << Santa.new("george", "male", "Latino")
-santas << Santa.new("andy", "bigender", "white")
-santas << Santa.new("stephanie", "female", "Japanese")
-santas << Santa.new("laura", "female", "prefer not to say")
-santas << Santa.new("jaden", "gender fluid", "Mystical Creature (unicorn)")
-santas << Santa.new("Bob", "N/A", "N/A")
-#p santas
-p santas.length #7
-
-timing = 0
+example_names = ["michael", "james", "trin", "pham"]
+example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
 2.times do
-  i = 0 #resets 1 on second times
-  if timing > 0
-    p santas[i].name
-    p santas[i].celebrate_birthday
-  end
-
-  until i >= santas.length
-    p santas[i].name
-    p santas[i].celebrate_birthday
-    i += 1
-  end
-   timing += 1
+  santas << Santa.new(example_names.shuffle[0], example_genders.shuffle[0], example_ethnicities.shuffle[0])
 end
 
-#doesn't work on existing santas because enumerator issues...
-rudy = Santa.new("rudy", "male", "asian")
-rudy.get_mad_at("Prancer") #no method error
-#calling my setter method
-p rudy.name = "homer"
-p rudy.age
-p rudy.ethnicity
+p santas
+#i have a setter - yet this errors out.
+#p santas.name
+
+# ## OFFICE HOURS
+# # ['a','b','c','d','e']
+
+# class Santa
+
+#   def initialize(name, gender, ethnicity)
+#     p "Initializing Santa instance..."
+#     @name = name
+#     @gender = gender
+#     @ethnicity = ethnicity
+#     @age = 0
+#     @reindeer_rank = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+#   end
+
+#   def name
+#     @name
+#   end
+
+#   def speak
+#     p "#{@name}: 'Ho, ho, ho! Haaaappy holidays!'"
+#   end
+
+#   def eat_milk_and_cookies
+#     p "#{@name} thanks you for that great plate of snickerdoodles!"
+#   end
+
+#   def celebrate_birthday
+#     @age += 1
+#   end
+
+#   def get_mad_at(reindeer_name)
+#     # ruby move array position
+#     # move element array ruby
+#     #@reindeer_rank.insert(-1, #@reindeer_rank.delete_at(@reindeer_rank.index(reindeer_name)))
+#     p @reindeer_rank << @reindeer_rank[@reindeer_rank.index(reindeer_name)]
+#     this one deletes both the old and the new name
+#     p @reindeer_rank.delete(reindeer_name)
+#     # http://ruby-doc.org/core-2.2.0/Array.html#method-i-delete
+
+#  # why does this pickup only 1 and not the new one at position last?
+#     p @reindeer_rank.index(reindeer_name)
+#     p @reindeer_rank.delete_at(@reindeer_rank.index(reindeer_name))
+# #    p @reindeer_rank
+#   end
+
+# #setter method
+#   def gender=(new_gender)
+#     @gender= new_gender
+#   end
+
+#   def name=(new_name)
+#     @name = new_name
+#   end
+
+# #getter methods
+#   def age
+#     @age
+#   end
+
+#   def ethnicity
+#     @ethnicity
+#   end
+
+# end
+# #testing release 0
+# nelson = Santa.new("nelson", "male", "asian")
+# # nelson.eat_milk_and_cookies
+# # nelson.speak
+
+# # santas = []
+# # santas << Santa.new("james", "male", "black")
+# # santas << Santa.new("george", "male", "Latino")
+# # santas << Santa.new("andy", "bigender", "white")
+# # santas << Santa.new("stephanie", "female", "Japanese")
+# # santas << Santa.new("laura", "female", "prefer not to say")
+# # santas << Santa.new("jaden", "gender fluid", "Mystical Creature (unicorn)")
+# # santas << Santa.new("Bob", "N/A", "N/A")
+# # #p santas
+# # p santas.length #7
+
+# # timing = 0
+# # 2.times do
+# #   i = 0 #resets 1 on second times
+# #   if timing > 0
+# #     p santas[i].name
+# #     p santas[i].celebrate_birthday
+# #   end
+
+# #   until i >= santas.length
+# #     p santas[i].name
+# #     p santas[i].celebrate_birthday
+# #     i += 1
+# #   end
+# #    timing += 1
+# # end
+
+# # #doesn't work on existing santas because enumerator issues...
+# # rudy = Santa.new("rudy", "male", "asian")
+# # rudy.get_mad_at("Prancer") #no method error
+# # #calling my setter method
+# # p rudy.name = "homer"
+# # p rudy.age
+# # p rudy.ethnicity
