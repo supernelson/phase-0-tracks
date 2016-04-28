0c833ff57f07b9a5008e35440928b069e1e3b325
Nelson Chen <nelsonchen1020@gmail.com>
2016-04-28 10:52:11 -0700

    initial commit for solo challenge 6.4 release 0

---
+++ ruby/solo.rb
@@ -0,0 +1,15 @@
+class TechCompanies
+  #age=>integer / salary=>hash / location=>string
+  attr_accessor :age, :salary, :location
+
+  def initialize
+    @age = age
+    @salary = salary
+    @location = location
+  end
+
+  def stock(movement)
+  end
+
+  def
+end
