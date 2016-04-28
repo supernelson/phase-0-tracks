31e4400a2b32fa2c1f0e0cdac8b767aca1f5ee70
Nelson Chen <nelsonchen1020@gmail.com>
2016-04-28 11:02:58 -0700

    added release 0 psuedocode and release 1 attr_accessor & attr_reader & initialize.

--- ruby/solo.rb
+++ ruby/solo.rb
@@ -1,15 +1,26 @@
+#class of Big Technology Companies
 class TechCompanies
-  #age=>integer / salary=>hash / location=>string
+  #attributes include: age=>integer / salary=>hash / location=>string
+  #reader includes website
   attr_accessor :age, :salary, :location
+  attr_reader :website
 
+#method to initialize the characteristics
   def initialize
     @age = age
     @salary = salary
     @location = location
   end
 
+#method to track behavior of stock price
   def stock(movement)
   end
 
-  def
+#method to track behavior of media news
+  def news(intensity)
+  end
+
+#method to track tech trends
+  def trends
+  end
 end
