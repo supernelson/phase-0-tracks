4731ff526b7818a2695bc07b20d3dc467ef1adde
Nelson Chen <nelsonchen1020@gmail.com>
2016-05-04 16:56:06 -0700

    working on reversing 'hello' in javascript, bug for undefine.

---
+++ js/explore.js
@@ -0,0 +1,14 @@
+// create a string variable "hello"
+// loop through each letter
+// start an empty string variable
+// current letter variable
+// length of string - index = new index position
+// add
+var string = 'hello';
+var new_string= '';
+for (i=0; i<=string.length; i++) {
+  new_string += string[(string.length)-[i]];
+  console.log(new_string);
+}
+
+// undefine bug
