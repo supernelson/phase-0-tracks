c4151ffc61fb69d27368465406f07fe11fff7c4d
Nelson Chen <nelsonchen1020@gmail.com>
2016-05-04 17:01:15 -0700

    added function & user interface

--- js/explore.js
+++ js/explore.js
@@ -4,11 +4,16 @@
 // current letter variable
 // length of string - index = new index position
 // add
-var string = 'hello';
-var new_string= '';
-for (i=0; i<=string.length; i++) {
-  new_string += string[(string.length)-[i]];
-  console.log(new_string);
+
+function reverse() {
+  var string = 'hello';
+  var new_string= '';
+  for (i=0; i<=string.length; i++) {
+    new_string += string[(string.length)-[i]];
+    console.log(new_string);
+  }
 }
 
-// undefine bug
+console.log(reverse());
+// bug undefined printing out begin & end
+// bug
