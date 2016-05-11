0848bb207636125f7ce73fde178cf9d75f13a069
Nelson Chen <nelsonchen1020@gmail.com>
2016-05-11 16:21:12 -0700

    completed 7.3 late, was sick :(

--- js/algos.js
+++ js/algos.js
@@ -6,18 +6,15 @@
 // save the length into a new variable new_length
 
 function big_word(array) {
-  var longest_words = ''
-  var current_words = ''
+  var longest_words = '';
+  var current_words = '';
   for (var i=0; i<array.length; i++) {
-    current_words = array[i]
+    current_words = array[i];
     if (current_words.length > longest_words.length) {
-      longest_words = current_words
-      longest_words
-    }
-    else {
-      longest_words
+      longest_words = current_words;
     }
   }
+  return longest_words;
 }
 var words = ["long phrase","longest phrase","longer phrase"]
 console.log(big_word(words))
@@ -29,18 +26,14 @@ console.log(big_word(words))
 // iterate through
 
 function names_ages(first, second) {
-  var keyOne = []
-  var keyTwo = []
-  var valueOne = []
-  var valueTwo = []
-  if ( keyOne == keyTwo || valueOne == valueTwo ) {
-    return true
+  if ( first.name == second.name || first.age == second.age ) {
+    return true;
   } else {
-    return false
+    return false;
   }
 }
 
-names_ages( {name: "Steven", age: 54}, {name: "Tamir", age: 54} )
+console.log(names_ages( {name: "Steven", age: 54}, {name: "Tamir", age: 54} ));
 
 // Release 2
 // create a function that takes an integer
@@ -48,5 +41,28 @@ names_ages( {name: "Steven", age: 54}, {name: "Tamir", age: 54} )
 // randomly generate lengths of each string within the array.
 // driver code executes 10 times: generates array, prints the array,feeds array to your "longest word" function, prints the result.
 
-// function createArray(integer) {
-// }
+function generate(number) {
+  var alphabet = "abcdefghijklmnopqrstuvwxyz";
+  var array = [];
+  // generate word length
+  for (var i=0; i < number; i++) {
+    var wordLength = Math.floor(Math.random()*10);
+    var word = ''
+    // generating the letters for the word
+    for (var x=0; x <= wordLength; x++) {
+      var letterIndex = Math.floor(Math.random()*26);
+      word += alphabet.charAt(letterIndex);
+    }
+    array.push(word);
+  }
+  return array;
+}
+console.log(generate(3));
+
+// Driver Code
+for (var i=0; i<=10; i++) {
+  var result = generate(2);
+  console.log(result);
+  // expecting array
+  console.log(big_word(result));
+}
